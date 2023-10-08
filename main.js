40e463fdd956db5c3b9085f8537c36d55da20a50
hisham3243 <hisham_3243@proton.me>
2023-10-08 06:37:41 +0300

    ERRORS

--- main.js
+++ main.js
@@ -2,58 +2,95 @@
 let input = document.getElementById("in");
 let output = document.getElementById("out");
 let del = document.querySelector(".del");
-let left = document.querySelector(".lefty");
-let right = document.querySelector(".righty");
+let left = document.querySelector(".lefty button");
+let right = document.querySelector(".righty button");
 let nums = [...document.querySelectorAll(".num button")];
 let ops = [...document.querySelectorAll(".op button")];
 let equal = ops.pop();
-
-// Functions
-function add(a, b) {
-    // a = [...a.map((n) => +n)];
-    // let opertion = a.reduce((r, n) => r + n);
-    // return opertion;
-    return +a + +b;
-}
-
-function subtract(a, b) {
-    // a = [...a.map((n) => +n)];
-    // let opertion = a.reduce((r, n) => r - n);
-    // return opertion;
-    return +a - +b;
-}
-
-function multiply(a, b) {
-    //     a = [...a.map((n) => +n)];
-    //     let opertion = a.reduce((r, n) => r * n);
-    //     return opertion;
-    return +a * +b;
-}
-
-function divide(a, b) {
-    // a = [...a.map((n) => +n)];
-    // let opertion = a.reduce((r, n) => r / n);
-    // return opertion;
-    return +a / +b;
-}
-
-// function mod(a) {
-//     a = [...a.map((n) => +n)];
-//     let opertion = a.reduce((r, n) => r % n);
-//     return opertion;
-// }
-
 let keyRegex = /^[0-9 \/ * \- \+ \( \) .]/;
+let numRegex = /[0-9]/;
+let opRegex = /[\*\+\/\-]/;
+
+//?
+function operateOnEntry(userEntry) {
+    // This is where the calculations occur when hitting =
+    const operationsMD = ["*", "/"];
+    let indexOfOperand;
+    let operation;
+
+    while (userEntry.includes("*") || userEntry.includes("/")) {
+        let i = 0;
+        if (!userEntry.includes("*")) {
+            i++;
+        }
+        indexOfOperand = userEntry.indexOf(operationsMD[i]);
+        userEntry = calculatorOperations.calculationSequence(
+            operationsMD[i],
+            indexOfOperand,
+            userEntry
+        );
+    }
+    while (userEntry.includes("+") || userEntry.includes("-")) {
+        indexOfOperand = 1;
+        userEntry = calculatorOperations.calculationSequence(
+            userEntry[1],
+            indexOfOperand,
+            userEntry
+        );
+    }
+    return userEntry;
+}
+
+let calculatorOperations = {
+    "*": (arg1, arg2) => {
+        return arg1 * arg2;
+    },
+    "/": (arg1, arg2) => {
+        return arg1 / arg2;
+    },
+    "+": (arg1, arg2) => {
+        return arg1 + arg2;
+    },
+    "-": (arg1, arg2) => {
+        return arg1 - arg2;
+    },
+    returnIndexOfEntry: (index, userEntry) => {
+        let arg1 = Number(userEntry[index - 1]);
+        let arg2 = Number(userEntry[index + 1]);
+        return [arg1, arg2];
+    },
+    returnSpliced: (index, newTotal, userEntry) => {
+        userEntry.splice(index - 1, 3, newTotal);
+        return userEntry;
+    },
+    calculationSequence: (operation, indexOfOperand, userEntry) => {
+        let getArgs = calculatorOperations.returnIndexOfEntry(
+            indexOfOperand,
+            userEntry
+        );
+        let newTotalForEntry = calculatorOperations[operation](
+            getArgs[0],
+            getArgs[1]
+        );
+        let newUserEntry = calculatorOperations.returnSpliced(
+            indexOfOperand,
+            newTotalForEntry,
+            userEntry
+        );
+        return newUserEntry;
+    },
+};
+//?
 
 // input keyboard
 window.addEventListener("keydown", (e) => {
-    input.blur(); // block insterting two digits on a keypress
+    input.blur(); //! block insterting two digits on a keypress
     if (keyRegex.test(e.key)) {
         input.value += e.key;
     } else if (e.key == "Backspace" || e.key == "Delete") {
         input.value = input.value.slice(0, -1);
     } else if (e.key == "Enter") {
-        equal.click(); // result function
+        equal.click(); // generate result
     }
 });
 
@@ -77,142 +114,34 @@ right.addEventListener("click", (e) => {
 
 del.onclick = () => (input.value = input.value.slice(0, -1));
 
-let opRegex = /\*\+\/\-/g;
-// let regex = /[0-9]+\+\/\-\*[0-9]/;
-// Calculating Result
+// Hitting =
 equal.addEventListener("click", (e) => {
-    let fin = [];
-    console.log(input.value.split(/[0-9]/g));
-    console.log(input.value.split(/\+|\/|\-|\*/));
-    // !
-    let ops = input.value.split(/[0-9]/);
-    ops.pop();
-    ops.shift();
-    console.log(ops);
-    // !
-    ops.forEach((o) => (o.length > 1 ? "error" : true));
+    let nums = input.value.split(opRegex);
+    let ops = input.value.split(numRegex).filter((str) => str !== "");
 
-    for (i = 0; i < ops.length; i++) {
-        if (ops[i].length > 1) {
-            break;
-        } else {
-            nums = input.value.split(/\+|\/|\-|\*/);
-            console.log(nums);
-            switch (ops[i]) {
-                case "+":
-                    fin.push(add(nums[i], nums[i + 1]));
-                    console.log("add", fin);
-                    break;
-                case "-":
-                    fin.push(subtract(nums[i], nums[i + 1]));
-                    console.log("sub", fin);
-                    break;
-                case "*":
-                    fin.push(multiply(nums[i], nums[i + 1]));
-                    console.log("mul", fin);
-                    break;
-                case "/":
-                    fin.push(divide(nums[i], nums[i + 1]));
-                    console.log("div", fin);
-                    break;
+    //? Error handling
+    if (ops.length + 1 == nums.length && nums[nums.length - 1] == "") {
+        output.textContent = "ERROR";
+    } else {
+        // convert string to number
+        nums = nums.map((num) => +num);
+
+        // putting it all together
+        for (let i = 0; i < ops.length; i++) {
+            for (let j = 1; j < nums.length - i; j += 2) {
+                nums.splice(j, 0, ops[0]);
+                ops.shift();
             }
+            break;
+        }
+
+        let result = operateOnEntry(nums);
+
+        //? Error handling
+        if (Number.isNaN(+result)) {
+            output.textContent = "ERROR";
+        } else {
+            output.textContent = result;
         }
     }
-
-    output.textContent = add(input.value.split("+"));
-    // console.log(input.value.split(/[0-9]+(\+\/\-\*)*[0-9]/));
 });
-
-//? Reduce
-
-
-// !!!!!
-// function operateOnEntry(userEntry) {
-//     let indexOfOperand;
-//     let operation;
-//     Object.keys(calculatorOperations).forEach(function(functionName) {
-//       while (userEntry.includes(functionName)) {
-//         indexOfOperand = userEntry.indexOf(functionName);
-//         userEntry = calculationSequence(functionName, indexOfOperand, userEntry);
-//       }
-//     });
-//     return userEntry;
-//   }
-//   const returnIndexOfEntry = (index, userEntry) => {
-//     const arg1 = Number(userEntry[index - 1]);
-//     const arg2 = Number(userEntry[index + 1]);
-//     return [arg1, arg2];
-//   };
-//   const returnSpliced = (index, newTotal, userEntry) => {
-//     userEntry.splice((index - 1), 3, newTotal);
-//     return userEntry;
-//   };
-//   const calculationSequence = (operation, indexOfOperand, userEntry) => {
-//     const getArgs = returnIndexOfEntry(indexOfOperand, userEntry);
-//     const newTotalForEntry = calculatorOperations[operation](getArgs[0], getArgs[1]);
-//     const newUserEntry = returnSpliced(indexOfOperand, newTotalForEntry, userEntry);
-//     return newUserEntry;
-//   }
-//   const calculatorOperations = {
-//     'x': (arg1, arg2) => arg1 * arg2,
-//     '/': (arg1, arg2) => arg1 / arg2,
-//     '+': (arg1, arg2) => arg1 + arg2,
-//     '-': (arg1, arg2) => arg1 - arg2
-//   };
-//   var userEntry = [3, '+', 3, 'x', 3];
-//   console.log(operateOnEntry(userEntry));
-
-// !!!!!!!!!!
-// function operateOnEntry(userEntry) {
-//     //this is where the calculations occur when hitting =
-//     const operationsMD = ['x', '/'];
-//     let indexOfOperand;
-//     let operation;
-    
-//     while (userEntry.includes('x') || userEntry.includes('/')) {
-//         let i = 0;
-//         if (!userEntry.includes('x')) {
-//             i++;
-//         }
-//         indexOfOperand = userEntry.indexOf(operationsMD[i]);
-//         userEntry = calculatorOperations
-//             .calculationSequence(operationsMD[i], indexOfOperand, userEntry);
-//     }
-//     while (userEntry.includes('+') || userEntry.includes('-')) {
-//         indexOfOperand = 1;
-//         userEntry = calculatorOperations
-//             .calculationSequence(userEntry[1], indexOfOperand, userEntry);
-//     }
-//     return userEntry;
-//     }
-    
-//     let calculatorOperations = {
-//     'x': (arg1, arg2) => {
-//         return arg1 * arg2;
-//     },
-//     '/': (arg1, arg2) => {
-//         return arg1 / arg2;
-//     },
-//     '+': (arg1, arg2) => {
-//         return arg1 + arg2;
-//     },
-//     '-': (arg1, arg2) => {
-//         return arg1 - arg2;
-//     },
-//     returnIndexOfEntry: (index, userEntry) => {
-//         let arg1 = Number(userEntry[index - 1]);
-//         let arg2 = Number(userEntry[index + 1]);
-//         return [arg1, arg2];
-//     },
-//     returnSpliced: (index, newTotal, userEntry) => {
-//         userEntry.splice((index - 1), 3, newTotal);
-//         return userEntry;
-//     },
-//     calculationSequence: (operation, indexOfOperand, userEntry) => {
-    
-//         let getArgs = calculatorOperations.returnIndexOfEntry(indexOfOperand, userEntry);
-//         let newTotalForEntry = calculatorOperations[operation](getArgs[0], getArgs[1]);
-//         let newUserEntry = calculatorOperations.returnSpliced(indexOfOperand, newTotalForEntry, userEntry);
-//         return newUserEntry;
-//     }
-//     };
